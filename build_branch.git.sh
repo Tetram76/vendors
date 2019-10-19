2184e469ff86cb2aba7d9e15f5295940e60c6c66
Tetram76 <Tetram76@users.noreply.github.com>
2019-10-19 18:23:37 +0200

    Update build_branch.git.sh

--- build_branch.git.sh
+++ build_branch.git.sh
@@ -1,10 +1,10 @@
 git checkout --orphan $1
 git rm -rf .
-echo $2 > .origin.url
+echo "$2" > .origin.url
 git add .origin.url
 git commit -m "Add $1 vendor branch"
 
-git remote add -t master $1 $2
+git remote add -t master $1 "`cat .origin.url`"
 git fetch -n $1
 git merge -X subtree=. --allow-unrelated-histories --no-commit $1/master
 
